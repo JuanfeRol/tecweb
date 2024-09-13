6aa7d9eb9b1852fbaa4791a75c9f2768bbd8a808
vf <juan.perezrold@alumno.buap.mx>
2024-09-12 19:24:11 -0600

    Se creo la funcion generacionRepetitiva en index/funcionex.php correspondiente al ejercicio 2

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -9,34 +9,18 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+        require('src/funciones.php');
+
+        if(isset($_GET['numero'])){
+        $num = $_GET['numero'];
+        multiplo($num);
         }
     ?>
-
-    <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
-    <br>
+    <h2>Ejercicio 2</h2>
+    <p> Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+        secuencia compuesta por: par - impar - par</p> 
     <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
-        {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
-        }
+        generacionRepetitiva();
     ?>
 </body>
 </html>
--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -13,7 +13,21 @@ function multiplo($num){
 
 
 function generacionRepetitiva(){
-    
+    #Generación de 3 números aleatorios hasta obtener una secuencia compuesta por: impar - par - impar
+    $contadorIteraciones = 0;
+    $flag = false;
+    while(!$flag){
+        $num1 = rand(1,1000);
+        $num2 = rand(1,1000);
+        $num3 = rand(1,1000);
+        $matriz[$contadorIteraciones] = array($num1, $num2, $num3); 
+        $contadorIteraciones++;
+        if($num1%2!=0 && $num2%2==0 && $num3%2!=0){
+            $flag = true;
+            echo 'R= La secuencia de números es: '.$num1.' - '.$num2.' - '.$num3.'<br>';
+        }
+    }
+    echo 'Se han necesitado '.$contadorIteraciones.' iteraciones para obtener la secuencia.<br>';
 }
 
 ?>
