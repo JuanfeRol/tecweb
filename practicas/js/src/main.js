abba6a6c038e0621126ab0d031878a27688435c0
vf <juan.perezrold@alumno.buap.mx>
2024-09-26 14:14:57 -0600

    Se realizo un ejercicio en clase y se descargon recursos necesarios en ejerciciosJS para realizar la actividad del dia

---
+++ practicas/js/src/main.js
@@ -0,0 +1,10 @@
+function getDatos(){
+    var nombre = window.prompt("Nombre: ", " ");
+    var edad = window.prompt("Edad: ", " ");
+
+    var div1 = document.getElementById("nombre");
+    div1.innerHTML = "<h3>Nombre: " + nombre + "</h3>";
+
+    var div2 = document.getElementById("edad");
+    div2.innerHTML = "<h3>Edad: " + edad + "</h3>";
+}   
